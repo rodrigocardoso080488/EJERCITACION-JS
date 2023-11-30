970a086a5fac8efe76a6e0b28e1c19be51efd41f
rodrigocardoso <rodrigocardoso080488@gmail.com>
2023-11-30 09:17:40 -0300

    Se comenzo a resolver el challenge 7

---
+++ js/challenge7.js
@@ -0,0 +1,7 @@
+Obtener la base y la altura de un triangulo, y el radio de un circulo. Mostrar por consola el area del triangulo y del circulo con un mensaje detallando cual es cual.
+
+Recordar:
+--Area de triangulo: (base x altura)/2
+--Area del circulo: π(pi) x radio^2
+
+*Para aumentar la dificultad lograr que el usuario brinde los datos necesarios para calcular (interactuando) y mostrarle en pantalla el resultado.
